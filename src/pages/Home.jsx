6f9bf2958ed5d0b4dba97b2add6085faa74d03c5
harshaaa5050 <harshaaa0505@gmail.com>
2026-03-10 12:17:26 +0530

    feat: Add testimonials section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Hero from '../components/Hero.jsx'
 import About from '../components/About.jsx'
 import Courses from '../components/Courses.jsx'
 import Blog from '../components/Blog.jsx'
+import Testimonials from '../components/Testimonials.jsx'
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
         <About />
         <Courses />
         <Blog />
+        <Testimonials />
     </>
   )
 }
